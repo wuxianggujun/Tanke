29ea6a6009a60b5fbe3232eb6d68f7beca9a35de
3344207732@qq.com <3344207732@qq.com>
2021-12-14 11:28:51 +0800

    创建了精灵类

---
+++ src/main/java/com/wuxianggujun/tanke/sprite/Background.java
@@ -0,0 +1,12 @@
+package com.wuxianggujun.tanke.sprite;
+
+import com.wuxianggujun.tanke.scene.GameScene;
+import javafx.scene.image.Image;
+
+public class Background extends Sprite{
+
+    public Background(Image image, double x, double y, double width, double height, GameScene gameScene) {
+        super(image, x, y, width, height, gameScene);
+    }
+
+}
